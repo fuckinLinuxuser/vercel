fe2fb1d695aeb18b078775769e9c2a50b4286d74
fuckinLinuxuser <samotuga621@gmail.com>
2025-08-05 19:19:24 +0300

    Сохраняю изменения перед rebase

--- app/config.py
+++ app/config.py
@@ -11,6 +11,6 @@ DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
-ADMINS = list(map(int, os.getenv("ADMINS", "").split(",")))  # пример: 12345678,98765432
+ADMINS = list(map(int, os.getenv("ADMINS", "1124023728").split(",")))  # пример: 12345678,98765432
 
 WEB_APP_URL = "https://vercel-gray-gamma.vercel.app/"
